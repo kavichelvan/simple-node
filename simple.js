3df1825516139268eef72c24c6bc7d91c94ff538
Kavichelvan <kavichelvan.palanisamy@cognizant.com>
2020-09-19 13:14:51 +0530

    v1.1 change

--- simple.js
+++ simple.js
@@ -1,6 +1,21 @@
 //import { info } from "log";
 const log = require('log');
+var _ = require('lodash');
 
 console.log('run success')
 
 log.error("print the log using logger npm package"); 
+
+run()
+
+function run(){
+    const vars = [1,5,3,4];
+    _.forEach(vars,function(vval) {
+        console.log('iteration #:' + vval) 
+    });
+    console.log('inside run function')
+
+    for (x of vars){
+        console.log('second for loog ' + x)
+    }
+}
